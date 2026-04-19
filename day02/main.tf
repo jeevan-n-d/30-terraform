ef3caf660226658f8e80db9200e0164ec51cbd88
JEEVAN N D <jeevanrajeshgowda@gmail.com>
2026-04-19 19:08:42 +0530

    Update main.tf

--- day02/main.tf
+++ day02/main.tf
@@ -2,12 +2,13 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.0"
+      version = "~> 5.0"
     }
   }
+
+  required_version = ">= 1.0"
 }
 
-# Configure the AWS Provider
 provider "aws" {
   region = "ap-south-1"
 }
